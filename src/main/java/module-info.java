8f459783a7622fa375f1cf3283a052a710316f7b
LENOVO <hfz.snyl@gmail.com>
2025-03-30 01:06:02 +0300

    module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,14 @@
 module com.hafizesenyil.ibb_education_javafx {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
+    //requires javafx.web;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
+    //requires eu.hansolo.tilesfx;
 
     opens com.hafizesenyil.ibb_education_javafx to javafx.fxml;
     exports com.hafizesenyil.ibb_education_javafx;
