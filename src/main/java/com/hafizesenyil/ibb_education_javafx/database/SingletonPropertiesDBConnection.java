b65059436773aa7e7ad76f7e6973ddfb50780750
LENOVO <hfz.snyl@gmail.com>
2025-04-06 18:09:36 +0300

    UserDTO için çalıştırma blok kapatma

--- src/main/java/com/hafizesenyil/ibb_education_javafx/database/SingletonPropertiesDBConnection.java
+++ src/main/java/com/hafizesenyil/ibb_education_javafx/database/SingletonPropertiesDBConnection.java
@@ -1,4 +1,39 @@
 package com.hafizesenyil.ibb_education_javafx.database;
 
+import com.hafizesenyil.ibb_education_javafx.utils.SpecialColor;
+import  java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class SingletonPropertiesDBConnection {
-}
+
+    // Field
+    // Database  Information Data
+    private static String URL = "jdbc:h2:./h2db/blog" + "AUTO_SERVER=TRUE";
+    private static String USERNAME = "sa";
+    private static String PASSWORD = "";
+
+    // Parametresiz Constructor (private ile dışarıdan erişilemez olmasını sağlamak)
+    private SingletonPropertiesDBConnection() {
+    }
+
+    //  For Database Constructor
+    private static Connection connection;
+
+    // Method
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                System.out.println(SpecialColor.GREEN + "Veritabani baglantisi basarili" + SpecialColor.RESET);
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+                System.out.println(SpecialColor.RED + "Veri tabani baglantisi basarisiz" + SpecialColor.RESET);
+                throw new RuntimeException("Veri tabani baglantisi basarisiz");
+            }
+
+        }
+        return connection;
+    }
+} // end class
