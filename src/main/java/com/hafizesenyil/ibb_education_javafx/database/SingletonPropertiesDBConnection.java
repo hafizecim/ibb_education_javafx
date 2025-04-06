ef2faf32cca42ea4873e66ed136035b3ffe7e3f9
LENOVO <hfz.snyl@gmail.com>
2025-04-06 18:41:40 +0300

    SingletonPropertiesDBConnection

--- src/main/java/com/hafizesenyil/ibb_education_javafx/database/SingletonPropertiesDBConnection.java
+++ src/main/java/com/hafizesenyil/ibb_education_javafx/database/SingletonPropertiesDBConnection.java
@@ -10,30 +10,57 @@ public class SingletonPropertiesDBConnection {
 
     // Field
     // Database  Information Data
-    private static String URL = "jdbc:h2:./h2db/blog" + "AUTO_SERVER=TRUE";
+    //private static String URL = "jdbc:h2:./h2db/user_management" + "AUTO_SERVER=TRUE";
+    private static String URL = "jdbc:h2:~/h2db/user_management" + "AUTO_SERVER=TRUE";
     private static String USERNAME = "sa";
     private static String PASSWORD = "";
 
+    // Singleton Design pattern
+    private static SingletonPropertiesDBConnection instance;
+    private Connection connection;
+
     // Parametresiz Constructor (private ile dışarıdan erişilemez olmasını sağlamak)
     private SingletonPropertiesDBConnection() {
+        try {
+            // JDBC Yükle
+            Class.forName("org.h2.Driver");
+            this.connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Veritabanı bağlantısı başarılı");
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            System.out.println(SpecialColor.RED + "Veri tabani baglantisi basarisiz" + SpecialColor.RESET);
+            throw new RuntimeException("Veritabanı bağlantısı başarısız!");
+        }
     }
 
-    //  For Database Constructor
-    private static Connection connection;
-
-    // Method
-    public static Connection getConnection() throws SQLException {
-        if (connection == null || connection.isClosed()) {
-            try {
-                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-                System.out.println(SpecialColor.GREEN + "Veritabani baglantisi basarili" + SpecialColor.RESET);
-            } catch (SQLException sqlException) {
-                sqlException.printStackTrace();
-                System.out.println(SpecialColor.RED + "Veri tabani baglantisi basarisiz" + SpecialColor.RESET);
-                throw new RuntimeException("Veri tabani baglantisi basarisiz");
-            }
-
+    // Singleton Design Instance
+    public static synchronized SingletonPropertiesDBConnection getInstance() {
+        if (instance==null){
+            instance=new SingletonPropertiesDBConnection();
         }
+        return instance;
+    }
+
+    //Bağlantı nesnesi çağırma (sağ klik getter and setter --> alt + Ins (0))
+    public Connection getConnection() {
         return connection;
     }
+
+    // Database kapatmak
+    public static void closeConnection()
+    {
+        if(instance!=null && instance.connection!=null){
+            try {
+                instance.connection.close();
+                System.out.printf(SpecialColor.RED + "Veri tabani baglantisi kapatildi" + SpecialColor.RESET);
+            }catch (SQLException e){
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    // Database Text
+    public static void main(String[] args){
+
+    }
+
 } // end class
