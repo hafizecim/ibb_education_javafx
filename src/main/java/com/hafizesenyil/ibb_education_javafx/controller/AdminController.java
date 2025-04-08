430147aa339112609efaa8d9b7ce9bfa1240dc06
LENOVO <hfz.snyl@gmail.com>
2025-04-08 18:42:40 +0300

    admin.fxml, AdminController

--- src/main/java/com/hafizesenyil/ibb_education_javafx/controller/AdminController.java
+++ src/main/java/com/hafizesenyil/ibb_education_javafx/controller/AdminController.java
@@ -1,5 +1,14 @@
 package com.hafizesenyil.ibb_education_javafx.controller;
-
+import javafx.event.ActionEvent;
 public class AdminController {
     // Injection
+    public void addUser(ActionEvent actionEvent) {
+    }
+
+    public void updateUser(ActionEvent actionEvent) {
+    }
+
+    public void deleteUser(ActionEvent actionEvent) {
+    }
+
 }
