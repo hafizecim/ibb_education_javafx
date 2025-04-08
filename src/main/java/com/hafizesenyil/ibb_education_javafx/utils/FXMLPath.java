ea497e1a2fb91821666e5a30542823762faaf88c
LENOVO <hfz.snyl@gmail.com>
2025-04-09 00:04:49 +0300

    FXMLPath, HelloApplication, module-info.java revize

--- src/main/java/com/hafizesenyil/ibb_education_javafx/utils/FXMLPath.java
+++ src/main/java/com/hafizesenyil/ibb_education_javafx/utils/FXMLPath.java
@@ -2,8 +2,8 @@ package com.hafizesenyil.ibb_education_javafx.utils;
 
 public class FXMLPath {
 
-    public static final String LOGIN = "/com/hamitmizrak/ibb_ecodation_javafx/view/login.fxml";
-    public static final String REGISTER = "/com/hamitmizrak/ibb_ecodation_javafx/view/register.fxml";
-    public static final String ADMIN = "/com/hamitmizrak/ibb_ecodation_javafx/view/admin.fxml";
-    public static final String USER_HOME = "/com/hamitmizrak/ibb_ecodation_javafx/view/hello-view.fxml";
+    public static final String LOGIN = "/com/hafizesenyil/ibb_education_javafx/view/login.fxml";
+    public static final String REGISTER = "/com/hafizesenyil/ibb_education_javafx/view/register.fxml";
+    public static final String ADMIN = "/com/hafizesenyil/ibb_education_javafx/view/admin.fxml";
+    public static final String USER_HOME = "/com/hafizesenyil/ibb_education_javafx/view/hello-view.fxml";
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,15 @@
-
 // Gerekli izinleri verdiğimiz yer
-module com.hafizesenyil.ibb_education_javafx {
+module com.hamitmizrak.ibb_ecodation_javafx {
 
     // JavaFX'in temel bileşenlerini kullanmak için gerekli modüller
     // JavaFX kontrol bileşenlerini (Button, Label, TextField vb.) kullanabilmek için gereklidir.
     requires javafx.controls;
 
-
     // JavaFX FXML dosyalarını (FXML UI tasarımları) yükleyebilmek için gereklidir.
     requires javafx.fxml;
 
     // WEB
-    // requires javafx.web;
+    //requires javafx.web;
 
     // #######################################################################################
     // UI geliştirme için kullanılan harici kütüphaneler
@@ -30,7 +28,7 @@ module com.hafizesenyil.ibb_education_javafx {
     // BootstrapFX, Bootstrap benzeri CSS stillerini JavaFX'e entegre eder.
     requires org.kordamp.bootstrapfx.core;
 
-    // #######################################################################################
+    // #############################################
     // Lombok kütüphanesi, Java'da getter, setter, constructor gibi metotları otomatik oluşturur.
     // Lombok, derleme zamanı (compile-time) kullanıldığı için "static" olarak eklenmiştir.
     requires static lombok;
@@ -38,59 +36,65 @@ module com.hafizesenyil.ibb_education_javafx {
     // JDBC ile veritabanı bağlantısı kurabilmek için gerekli modül
     // Java'daki SQL işlemlerini (Connection, Statement, ResultSet vb.) gerçekleştirebilmek için gereklidir.
     requires java.sql;
-
-    requires java.mail;
+    requires com.h2database;
+    requires jbcrypt;
     requires org.apache.poi.ooxml;
     requires org.apache.pdfbox;
-    requires jbcrypt;
-    requires com.h2database;
+    requires java.desktop;
+    requires java.mail;
+    //requires eu.hansolo.tilesfx;
 
     // #######################################################################################
     // Paket Erişimlerine İzin vermek
     // `opens` ifadesi, bir paketin runtime'da (çalışma zamanında) refleksiyon (reflection) kullanılarak erişilebilir olmasını sağlar.
     // Ana paket (Root package) açılıyor, böylece FXML dosyalarından erişilebilir.
-       opens com.hafizesenyil.ibb_education_javafx to javafx.fxml;
-
-    // Controller sınıfları FXML tarafından kullanılacağı için açılması gerekiyor.
-       opens com.hafizesenyil.ibb_education_javafx.controller to javafx.fxml;
+    opens com.hafizesenyil.ibb_education_javafx to javafx.fxml;
 
     // DTO (Data Transfer Object) paketinin içeriği, JavaFX bileşenleri ve Lombok tarafından erişilebilir olmalıdır.
-       opens com.hafizesenyil.ibb_education_javafx.dto to javafx.base, lombok;
+    opens com.hafizesenyil.ibb_education_javafx.dto to javafx.base, lombok;
+
+    // Controller sınıfları FXML tarafından kullanılacağı için açılması gerekiyor.
+    opens com.hafizesenyil.ibb_education_javafx.controller to javafx.fxml;
 
     // DAO (Data Access Object) sınıfları, SQL bağlantısı kullandığı için açılıyor.
-       opens com.hafizesenyil.ibb_education_javafx.dao to java.sql;
+    opens com.hafizesenyil.ibb_education_javafx.dao to java.sql;
 
     // Veritabanı bağlantısı sağlayan sınıfların da SQL modülüne açık olması gerekiyor.
-       opens com.hafizesenyil.ibb_education_javafx.database to java.sql;
+    opens com.hafizesenyil.ibb_education_javafx.database to java.sql;
 
-    // #######################################################################################
+    // #####################################################################
     // Paket dışa aktarmak
     // `exports` ifadesi, paketin diğer modüller tarafından erişilebilir olmasını sağlar.
-
-    // Ana paketi dış dünyaya açıyoruz. Diğer modüller bu paketin içeriğini kullanabilir.
-    exports com.hafizesenyil.ibb_education_javafx;
 
     // DAO sınıflarını dışarıya açıyoruz. Böylece başka modüller veritabanı işlemlerini çağırabilir.
     exports com.hafizesenyil.ibb_education_javafx.dao;
 
     // // Veritabanı bağlantı paketini dış dünyaya açıyoruz. Diğer modüller DB bağlantısını kullanabilir.
     exports com.hafizesenyil.ibb_education_javafx.database;
+
+    // Ana paketi dış dünyaya açıyoruz. Diğer modüller bu paketin içeriğini kullanabilir.
+    exports com.hafizesenyil.ibb_education_javafx;
+    opens com.hafizesenyil.ibb_education_javafx.utils to javafx.base, lombok;
 }
 
-// Default
- /*module com.hafizesenyil.ibb_education_javafx {
-     requires javafx.controls;
-     requires javafx.fxml;
-     //requires javafx.web;
+//Default
+/*
+module com.hafizesenyil.ibb_education_javafx {
+        requires javafx.controls;
+        requires javafx.fxml;
+        //requires javafx.web;
 
-     requires org.controlsfx.controls;
-     requires com.dlsc.formsfx;
-     requires net.synedra.validatorfx;
-     requires org.kordamp.ikonli.javafx;
-     requires org.kordamp.bootstrapfx.core;
-     //requires eu.hansolo.tilesfx;
+        requires org.controlsfx.controls;
+        requires com.dlsc.formsfx;
+        requires net.synedra.validatorfx;
+        requires org.kordamp.ikonli.javafx;
+        requires org.kordamp.bootstrapfx.core;
+        requires static lombok;
+        requires java.sql;
+        requires java.desktop;
+        //requires eu.hansolo.tilesfx;
 
-     opens com.hafizesenyil.ibb_education_javafx to javafx.fxml;
-     exports com.hafizesenyil.ibb_education_javafx;
- }
- }*/
+        opens com.hafizesenyil.ibb_education_javafx to javafx.fxml;
+        exports com.hafizesenyil.ibb_education_javafx;
+        }
+*/
